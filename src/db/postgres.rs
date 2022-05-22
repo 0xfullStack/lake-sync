cc0a1c12f15db9ec1d2258280ca3cf2a85200521
Lin <linshizai.me@gmail.com>
2022-05-22 23:29:06 +0800

    mirror update

--- src/db/postgres.rs
+++ src/db/postgres.rs
@@ -1,7 +1,5 @@
-use actix::{Actor, SyncContext};
 use diesel::pg::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
-use actix_web::{error, Error};
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
 type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
@@ -13,14 +11,4 @@ pub fn init_pool(database_url: &str) -> Result<PgPool, PoolError> {
 
 pub struct Database {
     pub pool: PgPool
-}
-
-impl Database {
-    pub fn get_connection(&self) -> Result<PgPooledConnection, Error> {
-        self.pool.get().map_err(|e| error::ErrorInternalServerError(e))
-    }
-}
-
-impl Actor for Database {
-    type Context = SyncContext<Self>;
 }
